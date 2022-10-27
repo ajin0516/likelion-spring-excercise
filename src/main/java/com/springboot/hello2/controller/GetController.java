c164386859b1d93138b5df5f24a61dd5803b5660
ajin0516 <dkwls7432@naver.com>
2022-10-27 22:33:06 +0900

    [GET] GetMapping, PathVariable 적용

--- src/main/java/com/springboot/hello2/controller/GetController.java
+++ src/main/java/com/springboot/hello2/controller/GetController.java
@@ -1,13 +1,25 @@
 package com.springboot.hello2.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 @RestController
+@RequestMapping("/api/v1/get-api")
 public class GetController {
 
     @RequestMapping("/hello")
-    public String hello(){
-        return "Hello world";
+    public String gethello(){
+        return "Hello";
+    }
+
+    @GetMapping("/name")
+    public String getName(){
+        return "Ajin";
+    }
+
+    @GetMapping("/variable/{variable}")
+    public String getVariable(@PathVariable String variable){
+        return variable;
     }
 }
